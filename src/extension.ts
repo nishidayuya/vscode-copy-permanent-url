47f6a24bc94e19dfe5da6d8b3acf053aa30b9a6a
Yuya.Nishida <yuya@j96.org>
2023-08-13 21:25:22 +0900

    feat: implement

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,37 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+import childProcess = require('child_process');
+import { promisify } from 'util';
+
+const execFile = promisify(childProcess.execFile);
+const l = (message: string) => console.log(`[copy-permanent-url] ${message}`);
+
 export function activate(context: vscode.ExtensionContext) {
+	const disposable = vscode.commands.registerCommand('copy-permanent-url.copyPermanentUrl', async () => {
+		const fileFullPath = vscode.window.activeTextEditor!.document.fileName;
+		const selection = vscode.window.activeTextEditor!.selection;
+		const startLineNumber = selection.start.line + 1;
+		const endLineNumber = selection.end.line + 1;
+		const args = [fileFullPath, startLineNumber.toString(), endLineNumber.toString()];
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "copy-permanent-url" is now active!');
+		l(`running dpu: args=${JSON.stringify(args)}`);
+		const {
+			stderr,
+			stdout,
+		} = await execFile('dpu', args);
+		l(`ran dpu: args=${JSON.stringify(args)}`);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('copy-permanent-url.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Copy permanent URL!');
+		if (stderr !== '') {
+			l('error occurred:');
+			l(stderr);
+			vscode.window.showInformationMessage(`error occurred: ${stderr}`);
+			return;
+		}
+
+		const permanentUrl = stdout.trimEnd();
+		l(`determined: permanentUrl=${permanentUrl}`);
+		vscode.env.clipboard.writeText(permanentUrl);
+		vscode.window.showInformationMessage(`Copied: ${permanentUrl}`);
 	});
 
 	context.subscriptions.push(disposable);
